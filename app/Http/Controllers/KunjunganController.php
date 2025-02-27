3af428c18dc55761a80c423e9425c0a5099c5db0
Aditya Harist <adityaharisty019@gmail.com>
2025-02-27 09:18:31 +0700

    Menambahkan controller dan progress authentification

--- app/Http/Controllers/KunjunganController.php
+++ app/Http/Controllers/KunjunganController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 // pengontrol view, database, route untuk pengunjung termasuk tampilan dashboard admin
-class PengunjungController extends Controller
+class KunjunganController extends Controller
 {
     //
 }
